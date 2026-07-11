d1683b0595c45b6559042f616ba2c4c8a0da4c2e
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2026-07-11 17:04:33 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import os
 
 
 def move_file(command: str) -> None:
-    commands_list = command.split(" ")
+    commands_list = command.split()
     if len(commands_list) != 3 or commands_list[0] != "mv":
         return
 
@@ -12,9 +12,14 @@ def move_file(command: str) -> None:
     if commands_list[2][-1] == "/":
         commands_list[2] += commands_list[1].split("/")[-1]
 
-    if "/" in commands_list[2]:
-        folders = commands_list[2].split("/")
-        os.makedirs("/".join(folders[:-1]), exist_ok=True)
+    folders = commands_list[2].split("/")
+    new_path = ""
+    for i in range(len(folders) - 1):
+        new_path = os.path.join(new_path, folders[i])
+        try:
+            os.mkdir(new_path)
+        except OSError:
+            pass
 
     try:
         with (
